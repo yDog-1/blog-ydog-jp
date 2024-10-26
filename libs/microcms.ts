21a61356281d12d0356d63b3af71dd0e364e261f
yDog <ydog-1@outlook.jp>
2024-10-26 17:54:38 +0900

    fix: キャッシュをSSG向けに変更

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -1,95 +1,95 @@
-import { Blog } from "@/types/Blog";
-import { Content } from "@/types/Content";
+import type { Blog } from "@/types/Blog";
+import type { Content } from "@/types/Content";
 import { createClient } from "microcms-js-sdk";
 import type {
-  CustomRequestInit,
-  MicroCMSDate,
-  MicroCMSQueries,
+	CustomRequestInit,
+	MicroCMSDate,
+	MicroCMSQueries,
 } from "microcms-js-sdk";
 import { notFound } from "next/navigation";
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+	throw new Error("MICROCMS_SERVICE_DOMAIN is required");
 }
 
 if (!process.env.MICROCMS_API_KEY) {
-  throw new Error("MICROCMS_API_KEY is required");
+	throw new Error("MICROCMS_API_KEY is required");
 }
 
 // API取得用のクライアントを作成
 export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.MICROCMS_API_KEY,
+	serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+	apiKey: process.env.MICROCMS_API_KEY,
 });
 
 // ブログ一覧を取得
 export async function getList(
-  queries?: MicroCMSQueries,
-  customRequestInit: CustomRequestInit = { cache: "no-store" }
+	queries?: MicroCMSQueries,
+	customRequestInit: CustomRequestInit = { cache: "force-cache" },
 ): Promise<{ contents: Content[]; totalCount: number }> {
-  try {
-    const { contents, totalCount } = await client
-      .getList<Blog>({
-        endpoint: "blog",
-        queries: { limit: 10, ...queries },
-        customRequestInit: customRequestInit,
-      })
-      .then((data) => {
-        return {
-          contents: data.contents.map((content) => {
-            return localDate(content);
-          }),
-          totalCount: data.totalCount,
-        };
-      });
-    return { contents: contents, totalCount: totalCount };
-  } catch (error) {
-    // ここのエラー処理は考える
-    console.error(error);
-    notFound();
-  }
+	try {
+		const { contents, totalCount } = await client
+			.getList<Blog>({
+				endpoint: "blog",
+				queries: { limit: 10, ...queries },
+				customRequestInit: customRequestInit,
+			})
+			.then((data) => {
+				return {
+					contents: data.contents.map((content) => {
+						return localDate(content);
+					}),
+					totalCount: data.totalCount,
+				};
+			});
+		return { contents: contents, totalCount: totalCount };
+	} catch (error) {
+		// ここのエラー処理は考える
+		console.error(error);
+		notFound();
+	}
 }
 
 // ブログの詳細を取得
 export async function getDetail(
-  contentId: string,
-  queries?: MicroCMSQueries,
-  customRequestInit: CustomRequestInit = { cache: "no-store" }
+	contentId: string,
+	queries?: MicroCMSQueries,
+	customRequestInit: CustomRequestInit = { cache: "force-cache" },
 ): Promise<Content> {
-  try {
-    const detailData = await client
-      .getListDetail<Blog>({
-        endpoint: "blog",
-        contentId,
-        queries,
-        customRequestInit: customRequestInit,
-      })
-      .then((data) => {
-        return localDate(data);
-      });
-    return detailData;
-  } catch (error) {
-    console.error(error);
-    notFound();
-  }
+	try {
+		const detailData = await client
+			.getListDetail<Blog>({
+				endpoint: "blog",
+				contentId,
+				queries,
+				customRequestInit: customRequestInit,
+			})
+			.then((data) => {
+				return localDate(data);
+			});
+		return detailData;
+	} catch (error) {
+		console.error(error);
+		notFound();
+	}
 }
 
 // UTCを現地時間に変換・published.dateに格納
 function localDate<T>(content: MicroCMSDate & Blog & T): Content {
-  const published = (() => {
-    try {
-      if (!content.publishedAt) {
-        throw `ブログID: ${content.id}\nエラー: 公開日が取得できませんでした。`;
-      }
-      const publishedDate = new Date(content.publishedAt);
-      const date = `${publishedDate.getFullYear()}/${
-        publishedDate.getMonth() + 1
-      }/${publishedDate.getDate()}`;
-      return date;
-    } catch (e) {
-      console.error(e);
-      return "YYYY/MM/DD";
-    }
-  })();
-  return { ...content, localPublishedAt: published };
+	const published = (() => {
+		try {
+			if (!content.publishedAt) {
+				throw `ブログID: ${content.id}\nエラー: 公開日が取得できませんでした。`;
+			}
+			const publishedDate = new Date(content.publishedAt);
+			const date = `${publishedDate.getFullYear()}/${
+				publishedDate.getMonth() + 1
+			}/${publishedDate.getDate()}`;
+			return date;
+		} catch (e) {
+			console.error(e);
+			return "YYYY/MM/DD";
+		}
+	})();
+	return { ...content, localPublishedAt: published };
 }
