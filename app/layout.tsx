a79466a0f9e006965db376140080f38dcf219762
yDog <ydog-1@outlook.jp>
2024-10-26 16:27:45 +0900

    feat: Googleアナリティクスを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import "./globals.scss";
 import { Header } from "@/components/layouts/Header";
 import { Footer } from "@/components/layouts/Footer";
 import { getBasicMetadata } from "./metadata";
+import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const ZenKakuGothicNew = Zen_Kaku_Gothic_New({
-  weight: "400",
-  subsets: ["latin"],
+	weight: "400",
+	subsets: ["latin"],
 });
 
 const url = process.env.VERCEL_URL
@@ -14,24 +16,27 @@ const url = process.env.VERCEL_URL
 	: "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(url),
-  ...(() => getBasicMetadata({ path: "/" }))(),
+	metadataBase: new URL(url),
+	...(() => getBasicMetadata({ path: "/" }))(),
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="ja">
-      <body
-        className={`${ZenKakuGothicNew.className} flex min-h-screen flex-col`}
-      >
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<head>
+				<GoogleAnalytics gaId="G-CCXN6BJ87D" />
+			</head>
+			<body
+				className={`${ZenKakuGothicNew.className} flex min-h-screen flex-col`}
+			>
+				<Header />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
