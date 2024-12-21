fbe9945e5939c1d3862ca152c6f5bc7c97576fdc
yDog <ydog-1@outlook.jp>
2024-12-21 16:16:09 +0900

    feat: サイトマップ自動生成とrobots.txtの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://blog.ydog.jp",
+  integrations: [tailwind(), sitemap()],
 });
